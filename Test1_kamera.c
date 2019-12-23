fd18a47c615f6f58f8a65e47c2fba08078fc1ef6
Kristian <2annihiliate_fermions@hotmail.no>
2019-12-23 19:21:44 +0100

    First commit

---
+++ Test1_kamera.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+init()
+
+}
+
+void init(){
+
+int number;
+
+printf("This is a test, write a number:");
+scanf(&number);
+
+printf("%d", number);
+
+}
